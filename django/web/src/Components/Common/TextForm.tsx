45e499426b5e8af539768e2fc73e88d47a8f811f
s-sonoda <s-sonoda@k-useful.jp>
2020-07-07 15:17:00 +0900

    add Material-UI

---
+++ django/web/src/Components/Common/TextForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { TextField } from '@material-ui/core';
+
+class TextForm extends React.Component {
+    render() {
+        return (
+            <TextField></TextField>
+        )
+    }
+}
+
+export default TextForm
--- django/web/src/main.tsx
+++ django/web/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TextForm from './Components/Common/TextForm';
+
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Change Getter 1 Switch ON!</h1>
+                <TextForm></TextForm>
             </div>
         );
     }
